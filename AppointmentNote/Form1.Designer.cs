ae059c671cefe099d5b3ff770506132a263755c8
jatuporn-cmd <jatuporn.pimp@sbacnon.ac.th>
2026-02-16 09:27:05 +0700

    Add project files.

---
+++ AppointmentNote/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace AppointmentNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAppointmentID = new System.Windows.Forms.Label();
+            this.txtAppointmentID = new System.Windows.Forms.TextBox();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.lblImportance = new System.Windows.Forms.Label();
+            this.cboImportance = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.cboStatus = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAppointmentID
+            // 
+            this.lblAppointmentID.AutoSize = true;
+            this.lblAppointmentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppointmentID.Location = new System.Drawing.Point(50, 50);
+            this.lblAppointmentID.Name = "lblAppointmentID";
+            this.lblAppointmentID.Size = new System.Drawing.Size(126, 20);
+            this.lblAppointmentID.TabIndex = 0;
+            this.lblAppointmentID.Text = "Appointment ID:";
+            // 
+            // txtAppointmentID
+            // 
+            this.txtAppointmentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAppointmentID.Location = new System.Drawing.Point(200, 47);
+            this.txtAppointmentID.Name = "txtAppointmentID";
+            this.txtAppointmentID.Size = new System.Drawing.Size(250, 26);
+            this.txtAppointmentID.TabIndex = 1;
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubject.Location = new System.Drawing.Point(50, 100);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(67, 20);
+            this.lblSubject.TabIndex = 2;
+            this.lblSubject.Text = "Subject:";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSubject.Location = new System.Drawing.Point(200, 97);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(400, 26);
+            this.txtSubject.TabIndex = 3;
+            // 
+            // lblImportance
+            // 
+            this.lblImportance.AutoSize = true;
+            this.lblImportance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImportance.Location = new System.Drawing.Point(50, 150);
+            this.lblImportance.Name = "lblImportance";
+            this.lblImportance.Size = new System.Drawing.Size(94, 20);
+            this.lblImportance.TabIndex = 4;
+            this.lblImportance.Text = "Importance:";
+            // 
+            // cboImportance
+            // 
+            this.cboImportance.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboImportance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboImportance.FormattingEnabled = true;
+            this.cboImportance.Items.AddRange(new object[] {
+            "Low",
+            "Medium",
+            "High"});
+            this.cboImportance.Location = new System.Drawing.Point(200, 147);
+            this.cboImportance.Name = "cboImportance";
+            this.cboImportance.Size = new System.Drawing.Size(200, 28);
+            this.cboImportance.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(50, 200);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(60, 20);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status:";
+            // 
+            // cboStatus
+            // 
+            this.cboStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboStatus.FormattingEnabled = true;
+            this.cboStatus.Items.AddRange(new object[] {
+            "Planned",
+            "Confirmed",
+            "Completed"});
+            this.cboStatus.Location = new System.Drawing.Point(200, 197);
+            this.cboStatus.Name = "cboStatus";
+            this.cboStatus.Size = new System.Drawing.Size(200, 28);
+            this.cboStatus.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(200, 270);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 40);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.LightYellow;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(320, 270);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 40);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(440, 270);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 40);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cboStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cboImportance);
+            this.Controls.Add(this.lblImportance);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.txtAppointmentID);
+            this.Controls.Add(this.lblAppointmentID);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "โปรแกรมบันทึกการนัดหมาย";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAppointmentID;
+        private System.Windows.Forms.TextBox txtAppointmentID;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label lblImportance;
+        private System.Windows.Forms.ComboBox cboImportance;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ComboBox cboStatus;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ AppointmentNote/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AppointmentNote
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // เริ่มทำงานเมื่อโหลดฟอร์ม
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // กำหนดค่าเริ่มต้นให้ ComboBox เพื่อไม่ให้ว่าง
+            if (cboImportance.Items.Count > 0) cboImportance.SelectedIndex = 0; // เลือก Low
+            if (cboStatus.Items.Count > 0) cboStatus.SelectedIndex = 0; // เลือก Planned
+        }
+
+        // ปุ่ม Add: ตรวจสอบและบันทึก
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Data Validation: ตรวจสอบค่าว่าง
+            if (string.IsNullOrWhiteSpace(txtAppointmentID.Text) || string.IsNullOrWhiteSpace(txtSubject.Text))
+            {
+                MessageBox.Show("กรุณากรอก Appointment ID และ Subject ให้ครบถ้วน",
+                                "ข้อผิดพลาด (Warning)",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                return; // หยุดการทำงาน ไม่แสดงผลสรุป
+            }
+
+            // ดึงค่าจาก Form
+            string id = txtAppointmentID.Text;
+            string subject = txtSubject.Text;
+            string importance = cboImportance.SelectedItem?.ToString() ?? "Low";
+            string status = cboStatus.SelectedItem?.ToString() ?? "Planned";
+
+            // เตรียมข้อความแสดงผล
+            string resultMessage = "=== บันทึกข้อมูลสำเร็จ ===\n\n" +
+                                   $"รหัสการนัดหมาย: {id}\n" +
+                                   $"หัวข้อ: {subject}\n" +
+                                   $"ความสำคัญ: {importance}\n" +
+                                   $"สถานะ: {status}";
+
+            // แสดง MessageBox
+            MessageBox.Show(resultMessage, "รายละเอียดการนัดหมาย", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // ปุ่ม Clear: ล้างข้อมูล
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAppointmentID.Clear();
+            txtSubject.Clear();
+            cboImportance.SelectedIndex = 0; // กลับไปค่าแรก
+            cboStatus.SelectedIndex = 0;     // กลับไปค่าแรก
+            txtAppointmentID.Focus();        // ให้เคอร์เซอร์ไปกระพริบที่ช่องแรก
+        }
+
+        // ปุ่ม Exit: ปิดโปรแกรม
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // ถามยืนยันก่อนปิด (Option เสริมเพื่อให้ดูสมบูรณ์ขึ้น)
+            if (MessageBox.Show("ต้องการออกจากโปรแกรมใช่หรือไม่?", "ยืนยัน", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
